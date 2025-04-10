7f8b80ce3b2a0312617e9bed72203a8ec539a31d
tugcemiray <168101793+tugcemiray@users.noreply.github.com>
2025-04-10 06:30:52 +0300

    .

--- mlx_utils.c
+++ mlx_utils.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tukaraca <tukaraca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 23:06:25 by tukaraca          #+#    #+#             */
+/*   Updated: 2025/04/10 06:27:30 by tukaraca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 int	key_hook(int keycode, t_game	*game)
 {
 	if (keycode == 65307)
-		free_game(game);
+		end(game);
 	else if (keycode == 119)
 		move_player(game, 0, -1);
 	else if (keycode == 115)
@@ -15,26 +27,26 @@ int	key_hook(int keycode, t_game	*game)
 	return (0);
 }
 
-static void	put_image(t_game	*game, void *img, int row, int col)
+static void	put_image(t_game	*game, void *img, int col, int row)
 {
-	mlx_put_image_to_window(game -> mlx, game -> win, img, row * 64, col * 64);
+	mlx_put_image_to_window(game -> mlx, game -> win, img, col * 64, row * 64);
 }
 
-static void	put_map_utils(t_game *game, int x, int y)
+static void	put_map_utils(t_game *game, int row, int col)
 {
 	char	c;
 
-	c = game->map[y][x];
+	c = game->map[row][col];
 	if (c == '1')
-		put_image(game, game->wall, x, y);
+		put_image(game, game->wall, col, row);
 	else if (c == 'P')
-		put_image(game, game->player, x, y);
+		put_image(game, game->player, col, row);
 	else if (c == 'E')
-		put_image(game, game->exit, x, y);
+		put_image(game, game->exit, col, row);
 	else if (c == 'C')
-		put_image(game, game->fish, x, y);
+		put_image(game, game->fish, col, row);
 	else if (c == '0')
-		put_image(game, game->floor, x, y);
+		put_image(game, game->floor, col, row);
 }
 
 static void	draw_map(t_game *game)
@@ -57,6 +69,11 @@ void	get_textures(t_game *game)
 
 	value = 64;
 	game->mlx = mlx_init();
+	if (!game->mlx)
+	err_msg("Error: mlx_init failed");
+	game->win = mlx_new_window(game->mlx, game->width * 64, game->height * 64, "so_long");
+	if (!game->win)
+		err_msg("Error: mlx_new_window failed");
 	game -> floor = mlx_xpm_file_to_image(game -> mlx,
 			"textures/floor.xpm", &value, &value);
 	game -> player = mlx_xpm_file_to_image(game -> mlx,
@@ -69,6 +86,8 @@ void	get_textures(t_game *game)
 			"textures/fish.xpm", &value, &value);
 	draw_map(game);
 }
+
+
 
 
 
